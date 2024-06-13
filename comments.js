75735fe9476119d1b6d3317018620f68cf43185b
Christina <138369200+shukfanchung@users.noreply.github.com>
2024-06-13 04:53:22 +0100

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,15 +1,9 @@
-// create web server
-// 1. load http module
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
+// Create web server
+const express = require("express");
+const app = express();
 
-// 2. create server
-http.createServer(function (req, res) {
-    // 3. parse the request containing file name
-    var pathname = url.parse(req.url).pathname;
-    if (pathname == "/") {
-        pathname = "/index.html";
-    }
-})
+// Handle GET requests to /comments
+app.get("/comments", (req, res) => {
+  res.json([
+    {
+      id: 1,
